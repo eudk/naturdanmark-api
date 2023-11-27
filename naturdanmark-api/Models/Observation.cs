8aec61c73860dd82ddc94de1019a365493c0939b
Anders Fibiger Jensen <ande153d@edu.zealand.dk>
2023-11-27 12:33:36 +0100

    Modelklasse

---
+++ naturdanmark-api/Models/Observation.cs
@@ -0,0 +1,57 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace naturdanmark_api.Models
+{
+    public class Observation
+    {
+        public int ID { get; set; }
+
+        public string AnimalName { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public string Description { get; set; }
+
+        public string Længde { get; set; }
+
+        public string Bredde { get; set; }
+
+        public string? Billede { get; set; }
+
+        public void ValidateAnimalName()
+        {
+            if(AnimalName==null)
+            {
+                throw new ArgumentNullException();
+            }
+            if(AnimalName=="")
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+        }
+
+        public void ValidateLength()
+        {
+            if (Længde == null)
+            {
+                throw new ArgumentNullException();
+            }
+            if (Længde == "")
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+        }
+
+        public void ValidateBredde()
+        {
+            if (Bredde == null)
+            {
+                throw new ArgumentNullException();
+            }
+            if (Bredde == "")
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+        }
+    }
+}
---
+++ naturdanmark-apiTests/Models/ObservationTests.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using naturdanmark_api.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace naturdanmark_api.Models.Tests
+{
+    [TestClass()]
+    public class ObservationTests
+    {
+        [TestMethod()]
+        public void ValidateAnimalNameTest()
+        {
+            Observation obs1 = new Observation { AnimalName = "", ID = 1, Billede = null, Date = DateTime.Now, Description = null, Bredde = "breede", Længde = "base" };
+            Observation obs2 = new Observation { AnimalName = null, ID = 1, Billede = null, Date = DateTime.Now, Description = null, Bredde = "breede", Længde = "base" };
+            Observation obs3 = new Observation { AnimalName = "Animal", ID = 1, Billede = null, Date = DateTime.Now, Description = null, Bredde = "breede", Længde = "base" };
+            obs3.ValidateAnimalName();
+            Assert.ThrowsException<ArgumentNullException>(()=>obs2.ValidateAnimalName());
+            Assert.ThrowsException<ArgumentOutOfRangeException>(()=>obs1.ValidateAnimalName());
+            
+
+        }
+
+        [TestMethod()]
+        public void ValidateLengthTest()
+        {
+            Observation obs1 = new Observation { AnimalName = "animal", ID = 1, Billede = null, Date = DateTime.Now, Description = null, Bredde = "breede", Længde = null };
+            Observation obs2 = new Observation { AnimalName = "Animal", ID = 1, Billede = null, Date = DateTime.Now, Description = null, Bredde = "breede", Længde = "" };
+            Observation obs3 = new Observation { AnimalName = "Animal", ID = 1, Billede = null, Date = DateTime.Now, Description = null, Bredde = "breede", Længde = "base" };
+            obs3.ValidateLength();
+            Assert.ThrowsException<ArgumentNullException>(()=> obs1.ValidateLength());
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => obs2.ValidateLength());
+        }
+
+        [TestMethod()]
+        public void ValidateBreddeTest()
+        {
+            Observation obs1 = new Observation { AnimalName = "animal", ID = 1, Billede = null, Date = DateTime.Now, Description = null, Bredde = null, Længde = "base" };
+            Observation obs2 = new Observation { AnimalName = "animal", ID = 1, Billede = null, Date = DateTime.Now, Description = null, Bredde = "", Længde = "base" };
+            Observation obs3 = new Observation { AnimalName = "animal", ID = 1, Billede = null, Date = DateTime.Now, Description = null, Bredde = "breede", Længde = "base" };
+            obs3.ValidateBredde();
+            Assert.ThrowsException<ArgumentNullException>(() => obs1.ValidateBredde());
+            Assert.ThrowsException<ArgumentOutOfRangeException>( ()=> obs2.ValidateBredde());
+
+        }
+    }
+}
