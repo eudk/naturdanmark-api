3ab9963f6e2bd7a7c272e14c84876262a6038a82
Anders Fibiger Jensen <ande153d@edu.zealand.dk>
2023-12-05 14:50:34 +0100

    CoordinateController

---
+++ naturdanmark-api/Controllers/CoordinatesController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using naturdanmark_api.Models;
+using naturdanmark_api.Repositories;
+
+namespace naturdanmark_api.Controllers
+{
+    [ApiController]
+    [Route("Api/[Controller]")]
+    public class CoordinatesController:ControllerBase
+    {
+        private CoordinateRepo coordinateRepo = new();
+
+        public CoordinatesController(CoordinateRepo repo)
+        {
+            coordinateRepo = repo;
+                
+        }
+
+        [HttpGet("{id}")]
+        public Coordinates GetByID(int id)
+        {
+            return coordinateRepo.GetById(id);
+        }
+
+        [HttpPost]
+        public Coordinates Post(Coordinates cor)
+        {
+            return coordinateRepo.Add(cor);
+        }
+
+        [HttpPut]
+        public Coordinates Put(Coordinates cor,int id)
+        {
+            return coordinateRepo.Update(id, cor);
+        }
+    }
+}
