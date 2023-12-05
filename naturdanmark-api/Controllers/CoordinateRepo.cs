3be5a715d42b1020a591fe2ab215cc1d701c8e66
Anders Fibiger Jensen <ande153d@edu.zealand.dk>
2023-12-05 13:03:32 +0100

    TestFirst

---
+++ naturdanmark-api/Controllers/CoordinateRepo.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using naturdanmark_api;
+using naturdanmark_api.Models;
+
+namespace naturdanmark_api.Controllers
+{
+    public class CoordinateRepo
+    {
+        private readonly Hashtable Coordinates = new();
+
+        private int idcounter = 1;
+
+        public CoordinateRepo()
+        {
+            
+        }
+
+        public void GetById(int id)
+        {
+
+        }
+
+        public void Add()
+        {
+
+        }
+
+        public void Update()
+        {
+
+        }
+    }
+}
