41c6c501f898f1af2674f6701c2dd336d1a7f6b3
Anders Fibiger Jensen <ande153d@edu.zealand.dk>
2023-12-05 11:02:06 +0100

    Test failed

--- naturdanmark-api/Models/Coordinates.cs
+++ naturdanmark-api/Models/Coordinates.cs
@@ -3,14 +3,28 @@
     public class Coordinates
     {
 
+        public int DeviceID { get; set; }
+
+        public double Longitude { get; set; }
+
+        public double Latitude { get; set; }
+
+        public DateTime Date { get; set; }
+
         public void ValidateLongitude()
         {
-
+            if (Longitude > 180 || Longitude < -180)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
         }
 
         public void ValidateLatitude()
         {
-
+            if (Latitude > 90 || Latitude < -90)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
         }
     }
 }
--- naturdanmark-apiTests/Models/CoordinatesTest.cs
+++ naturdanmark-apiTests/Models/CoordinatesTest.cs
@@ -8,6 +8,7 @@ using System.Threading.Tasks;
 
 namespace naturdanmark_apiTests.Models
 {
+    [TestClass()]
     public class CoordinatesTest
     {
         [TestMethod]
