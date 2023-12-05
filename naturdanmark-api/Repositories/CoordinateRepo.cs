438b4ab53d9d126de5f82ffa473b0e0900c3a584
Anders Fibiger Jensen <ande153d@edu.zealand.dk>
2023-12-05 13:52:18 +0100

    Coordinates

--- naturdanmark-api/Repositories/CoordinateRepo.cs
+++ naturdanmark-api/Repositories/CoordinateRepo.cs
@@ -1,21 +1,26 @@
 ﻿using System.Collections;
-using naturdanmark_api;
 using naturdanmark_api.Models;
 
-namespace naturdanmark_api.Controllers
+namespace naturdanmark_api.Repositories
 {
     public class CoordinateRepo
     {
-        private readonly Hashtable Coordinates = new();
+        private readonly Hashtable Repo = new();
 
         private int idcounter = 1;
 
         public CoordinateRepo()
         {
-            
+            Repo.Add(1, new Coordinates { DeviceID = 1, Longitude = 170, Latitude = 45 });
+
         }
 
-        public void GetById(int id)
+        public void GetAll()
+        {
+
+        }
+
+        public void GetById()
         {
 
         }
---
+++ naturdanmark-apiTests/Repositories/CoordinateRepoTests.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using naturdanmark_api.Repositories;
+using naturdanmark_api.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace naturdanmark_apiTests.Repositories
+{
+    [TestClass()]
+    public class CoordinateRepoTests
+    {
+
+        [TestMethod()]
+        public void GetByIdTest()
+        {
+            CoordinateRepo repo = new CoordinateRepo();
+            Assert.AreEqual(repo.GetAll()[0], repo.GetById(1));
+            Assert.IsNull(repo.GetById(-1);
+        }
+
+        [TestMethod()]
+        public void AddTest()
+        {
+            CoordinateRepo repo = new CoordinateRepo();
+            int before = repo.GetAll().Count();
+            repo.Add(2,new Coordinates { DeviceID = 2, Latitude = 45, Longitude = 45, Date = DateTime.Now });
+            Assert.AreEqual(before + 1, repo.GetAll().Count);
+        }
+
+        [TestMethod()]
+        public void UpdateTest()
+        {
+            CoordinateRepo repo = new CoordinateRepo();
+            Coordinates newCor = new Coordinates{ DeviceID = 2, Longitude = 55, Latitude = 40, Date = DateTime.Now };
+            Coordinates Corold = repo.GetById(1);
+            Coordinates corupdated=repo.Update(1, new Coordinates { DeviceID = 1, Date = DateTime.Now, Longitude = 45, Latitude = 45 });
+            Assert.AreEqual(repo.GetAll()[0], corupdated);
+            Assert.IsNull(repo.Update(100000,newCor);
+
+        }
+    }
+}
