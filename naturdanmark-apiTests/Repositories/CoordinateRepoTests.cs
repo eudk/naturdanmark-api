4fe9a12b819d835c08dc020b6f8d11e261c17b63
Anders Fibiger Jensen <ande153d@edu.zealand.dk>
2023-12-06 20:35:27 +0100

    Testing changes

--- naturdanmark-apiTests/Repositories/CoordinateRepoTests.cs
+++ naturdanmark-apiTests/Repositories/CoordinateRepoTests.cs
@@ -34,10 +34,8 @@ namespace naturdanmark_apiTests.Repositories
         public void UpdateTest()
         {
             CoordinateRepo repo = new CoordinateRepo();
-            Coordinates newCor = new Coordinates{ DeviceID = 2, Longitude = 55, Latitude = 40, Date = DateTime.Now };
             Coordinates Corold = repo.GetById(1);
             Coordinates corupdated=repo.Update(1, new Coordinates { DeviceID = 1, Date = DateTime.Now, Longitude = 45, Latitude = 45 });
-            Coordinates cor = repo.GetById(1);
             Assert.AreNotEqual(Corold,corupdated);
             Assert.IsNull(repo.Update(10000,corupdated));
 
